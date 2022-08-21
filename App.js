55393463d1556500c6434fa60d159b760c20cff9
WeiLoon <weiloon.wong-prestatairep0@azqore.com>
2022-08-21 23:05:37 +0800

    initial commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text, 
+  View,
+} from 'react-native'; 
+ 
+
+const App = () => { 
+  return (
+    <SafeAreaView  > 
+       <View >
+           <Text>App Test for React Native</Text>
+        </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({ 
+});
+
+export default App;
